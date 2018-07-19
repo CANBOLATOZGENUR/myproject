0997d9b8104054ca12b8ae4e95643191cc79043d
Ozgenur Canbolat <canbolatozgenur@gmail.com>
2018-07-19 11:16:41 +0300

    update Databasehelper.java

--- app/src/main/java/ozgenurc/com/myproject/data/DatabaseHelper.java
+++ app/src/main/java/ozgenurc/com/myproject/data/DatabaseHelper.java
@@ -3,71 +3,52 @@ package ozgenurc.com.myproject.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import ozgenurc.com.myproject.data.myprojectContract.MahsulEntry;
-import ozgenurc.com.myproject.data.myprojectContract.DonemEntry;
-import ozgenurc.com.myproject.data.myprojectContract.GrupEntry;
-import ozgenurc.com.myproject.data.myprojectContract.UrunEntry;
-
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-     public static final String DATABASE_NAME="myproject.db";
-     public static final int DATABASE_VERSION=1;
+    public static final String DATABASE_NAME="asgenveritabani.db";
+    public static final int DATABASE_VERSION=1;
+    public static final String MAHSUL_TABLE_NAME="mahsul";
+    public static final String DONEM_TABLE_NAME="donem";
+    public static final String GRUP_TABLE_NAME="grup";
+    public static final String URUN_TABLE_NAME="urun";
 
-     public static final String TABLE_MAHSUL_CREATE=
-               " CREATE TABLE " + MahsulEntry.TABLE_NAME + " ("+
-                    MahsulEntry.KOD + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                    MahsulEntry.COLUMN_AD + " TEXT)";
-
-
-     public static final String TABLE_DONEM_CREATE=
-           " CREATE TABLE " + DonemEntry.TABLE_NAME + " ("+
-                  DonemEntry.KOD + " INTEGER PRIMARY KEY, "+
-                  DonemEntry.COLUMN_MKOD + " INTEGER, "+
-                  DonemEntry.COLUMN_AD + " TEXT)";
-
-     public static final String TABLE_GRUP_CREATE=
-             " CREATE TABLE " + GrupEntry.TABLE_NAME + " ("+
-                  GrupEntry.KOD + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                  GrupEntry.COLUMN_MKOD + " INTEGER, "+
-                  GrupEntry.COLUMN_DKOD + " INTEGER, "+
-                  GrupEntry.COLUMN_AD + " TEXT)";
-
-
-     public static final String TABLE_URUN_CREATE=
-          " CREATE TABLE " + UrunEntry.TABLE_NAME + " ("+
-                  UrunEntry.KOD + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                  UrunEntry.COLUMN_MKOD + " INTEGER, "+
-                  UrunEntry.COLUMN_DKOD + " INTEGER, "+
-                  UrunEntry.COLUMN_GKOD + " INTEGER, "+
-                  UrunEntry.COLUMN_AD + " TEXT, "+
-                  UrunEntry.COLUMN_DAMLADEKARA + " TEXT, "+
-                  UrunEntry.COLUMN_YAPRAKTANLT + " TEXT, "+
-                  UrunEntry.COLUMN_DOZDA + " TEXT, "+
-                  UrunEntry.COLUMN_UYGULAMASEKLI + " TEXT)";
-
-
+    public static final String TABLE_MAHSUL_CREATE= " CREATE TABLE "+MAHSUL_TABLE_NAME
+            + " (kod INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + " ad TEXT);";
+    public static final String TABLE_DONEM_CREATE= " CREATE TABLE "+DONEM_TABLE_NAME
+            + " (kod INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + " mkod INTEGER, "
+            + " ad TEXT);";
+    public static final String TABLE_GRUP_CREATE= " CREATE TABLE "+GRUP_TABLE_NAME
+            + " (kod INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + " mkod INTEGER, "
+            + " dkod INTEGER, "
+            + " ad TEXT);";
+    public static final String TABLE_URUN_CREATE= " CREATE TABLE "+URUN_TABLE_NAME
+            + " (kod INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + " mkod INTEGER, "
+            + " dkod INTEGER, "
+            + " gkod INTEGER, "
+            + " ad TEXT, "
+            + " damla_dekara TEXT, "
+            + " yapraktan_binlt TEXT, "
+            + " doz_da, TEXT);";
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         db.execSQL(TABLE_MAHSUL_CREATE);
         db.execSQL(TABLE_DONEM_CREATE);
         db.execSQL(TABLE_GRUP_CREATE);
         db.execSQL(TABLE_URUN_CREATE);
-
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldversion, int newversion) {
-
-        db.execSQL("DROP TABLE IF EXISTS " + MahsulEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + DonemEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + GrupEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + UrunEntry.TABLE_NAME);
-
+        db.execSQL("DROP TABLE IF EXISTS " +MAHSUL_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " +DONEM_TABLE_NAME);
+        db.execSQL("drop TABLE IF EXISTS " +GRUP_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " +URUN_TABLE_NAME);
         onCreate(db);
     }
 }
