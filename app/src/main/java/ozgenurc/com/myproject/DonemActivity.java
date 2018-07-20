f41b107ab52c9bb722e56f35354298f911d9251f
Ozgenur Canbolat <canbolatozgenur@gmail.com>
2018-07-20 14:23:50 +0300

    update

---
+++ app/src/main/java/ozgenurc/com/myproject/DonemActivity.java
@@ -0,0 +1,13 @@
+package ozgenurc.com.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donem);
+    }
+}
--- app/src/main/java/ozgenurc/com/myproject/MainActivity.java
+++ app/src/main/java/ozgenurc/com/myproject/MainActivity.java
@@ -1,24 +1,43 @@
 package ozgenurc.com.myproject;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import ozgenurc.com.myproject.data.myprojectContract;
 
 public class MainActivity extends AppCompatActivity {
+    ListView donemListesi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mahsulEkle();
+        donemListesi=(ListView)findViewById(R.id.listView);
+        //Donemlere işlevsellik kazandıracak metot
+        String[] donemler = getResources().getStringArray(R.array.donemListesi);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,donemler);
+        donemListesi.setAdapter(adapter);
+        donemListesi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DonemActivity.class);
+                String tiklanilanDonem = (String) donemListesi.getItemAtPosition(position);
+                intent.putExtra("donemIcerik", tiklanilanDonem);
+                startActivity(intent);
+            }
+        });
+
+
     }
     private void mahsulEkle(){
         ContentValues values=new ContentValues();
         values.put(myprojectContract.MahsulEntry.COLUMN_AD,"DOMATES");
-        values.put(myprojectContract.MahsulEntry.COLUMN_AD,"ANTEP FISTIĞI");
-        values.put(myprojectContract.MahsulEntry.COLUMN_AD,"MISIR");
-        values.put(myprojectContract.MahsulEntry.COLUMN_AD,"PAMUK");
         Uri _uri=getContentResolver().insert(myprojectContract.MahsulEntry.CONTENT_URI,values);
         Toast.makeText(this, "Eklendi:"+_uri,Toast.LENGTH_LONG).show();
     }
